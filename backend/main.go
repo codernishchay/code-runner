62e6136fa31351765c9325aed08820343715b00f
codernishchay <nishchay.vns2000@gmaiil.com>
2021-11-13 05:24:37 -0500

    .

--- backend/main.go
+++ backend/main.go
@@ -1,6 +1,11 @@
 package main 
-import "fmt"
-func main()  {
+import (
+	"fmt"
+    "github.com/gin-gonic/gin"
+)
+	func main()  {
+	app := gin.Default(); 
 	fmt.Println("package main"); 
+	fmt.Println(app); 
 	fmt.Println("how to run code ?")
 }
